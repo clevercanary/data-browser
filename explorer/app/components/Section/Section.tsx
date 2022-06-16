0fa9d41b3508b4470f2a0d415ea7471131a52eb3
Fran McDade <franmcdade@MiniMac.local>
2022-06-16 19:40:44 +1000

    Update description component to match refined mocks. #84.

--- explorer/app/components/Section/Section.tsx
+++ explorer/app/components/Section/Section.tsx
@@ -1,26 +1,69 @@
-import { Box } from "@mui/material";
-import React from "react";
-import { Text } from "../Text/Text";
+// Core dependencies
+import { Box, Collapse } from "@mui/material";
+import { CollapseProps } from "@mui/material/Collapse/Collapse";
+import React, { ReactNode, useEffect, useState } from "react";
 
-interface SectionProps {
+// App dependencies
+import { CollapseButton } from "./components/CollapseButton/collapseButton";
+import { SectionTitle } from "./components/SectionTitle/sectionTitle";
+import { useBreakpointHelper } from "../../hooks/useBreakpointHelper";
+
+interface Props {
+  children: ReactNode;
+  collapsable?: boolean;
   title?: string;
-  children: React.ReactNode | React.ReactNode[];
 }
 
-export const Section = ({ children, title }: SectionProps): JSX.Element => {
+export const Section = ({
+  children,
+  collapsable = false,
+  title,
+}: Props): JSX.Element => {
+  const mobile = useBreakpointHelper("down", "lg");
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const [transitionDuration, setTransitionDuration] =
+    useState<CollapseProps["timeout"]>(0);
+
+  const onToggleExpanded = () => {
+    setExpanded((expanded) => !expanded);
+  };
+
+  // Toggles expanded state on change of media breakpoint.
+  useEffect(() => {
+    setExpanded(!mobile);
+  }, [mobile]);
+
+  // Sets collapseTimeout state on change of media breakpoint.
+  // Delays setting transitionDuration state for mobile breakpoint to facilitate the immediate transition from
+  //  desktop to mobile.
+  useEffect(() => {
+    if (mobile) {
+      setTimeout(() => {
+        setTransitionDuration("auto");
+      }, 100);
+    } else {
+      setTransitionDuration(0);
+    }
+  }, [mobile]);
+
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      gap={(theme) => theme.spacing(2)}
-      padding={(theme) => theme.spacing(5)}
-    >
-      {title && (
-        <Text variant="text-body-large-500" customColor="ink">
-          {title}
-        </Text>
+    <Box display="flex" flexDirection="column" gap={2} padding={5}>
+      <Box display="flex" justifyContent="space-between">
+        <SectionTitle title={title} />
+        {mobile && collapsable && (
+          <CollapseButton
+            expanded={expanded}
+            onToggleExpanded={onToggleExpanded}
+          />
+        )}
+      </Box>
+      {collapsable ? (
+        <Collapse in={expanded} timeout={transitionDuration}>
+          {children}
+        </Collapse>
+      ) : (
+        children
       )}
-      <Box>{children}</Box>
     </Box>
   );
 };
