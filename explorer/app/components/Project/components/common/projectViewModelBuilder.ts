001b12bb768a972cf2623d51550930390bbda7a2
Fran McDade <franmcdade@MiniMac.local>
2022-06-23 14:50:13 +1000

    Linting.

--- explorer/app/components/Project/components/common/projectViewModelBuilder.ts
+++ explorer/app/components/Project/components/common/projectViewModelBuilder.ts
@@ -2,8 +2,8 @@
 import { ProjectResponse } from "../../../../models/responses";
 
 /**
- * Maps project description.
- * @param project - project response
+ * Maps project description from API response.
+ * @param project - Project response model return from API.
  * @returns string representation of project description
  */
 export function getProjectDescription(
