1cb783d5bd11a74e14232ee4d44aaba279ff3602
Mim Hastie <mim@clevercanary.com>
2022-06-27 16:20:18 -0700

    Linting.

--- explorer/app/models/viewModels.ts
+++ explorer/app/models/viewModels.ts
@@ -1,7 +1,7 @@
 import { ListResponseType } from "./responses";
 
 //Project
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- this data type cant be determined before hand
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- this data type can't be determined beforehand
 export interface DetailModel<T = any> {
   data?: T;
 }
