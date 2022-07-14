ffd596189311b01c5a4b3da1ee372b04fc2f57f2
Bruce Rodrigues <brucerodrigues6@gmail.com>
2022-07-14 09:48:15 -0300

    using router to get basePath. #204

--- explorer/app/components/common/StaticImage/staticImage.tsx
+++ explorer/app/components/common/StaticImage/staticImage.tsx
@@ -5,14 +5,10 @@
  * These images will be optimized at the build time by next-optimized-images
  */
 // Core dependencies
+import { useRouter } from "next/router";
 import React from "react";
 
 export type ImageSrc = string | undefined;
-
-/**
- * Constant to determine the base path used on next.config.mjs
- */
-export const BASE_PATH = "/explore/";
 
 export interface StaticImageProps {
   alt: string;
@@ -27,8 +23,15 @@ export const StaticImage = ({
   src,
   width,
 }: StaticImageProps): JSX.Element => {
+  const router = useRouter();
+
   return (
-    <img alt={alt} height={height} src={`${BASE_PATH}${src}`} width={width} />
+    <img
+      alt={alt}
+      height={height}
+      src={`${router.basePath}${src}`}
+      width={width}
+    />
   );
 };
 /* eslint-enable @next/next/no-img-element -- allow static images for logos etc. */
