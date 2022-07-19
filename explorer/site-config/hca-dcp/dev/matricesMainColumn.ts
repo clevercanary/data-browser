647f260f26825dc5e1062c5f85a830b04b400253
Fran McDade <franmcdade@MiniMac.hub>
2022-07-19 13:03:58 +1000

    Create generic detail page text component. #232.

--- explorer/site-config/hca-dcp/dev/matricesMainColumn.ts
+++ explorer/site-config/hca-dcp/dev/matricesMainColumn.ts
@@ -4,11 +4,13 @@ import { ComponentConfig } from "app/config/model";
 
 export const mainColumn: ComponentConfig[] = [
   {
-    component: C.Text,
+    component: C.TitledText,
     transformer: () => ({
-      children: "Main column. TBD",
-      customColor: "ink",
-      variant: "text-body-400-2lines",
+      text: [
+        "DCP Generated Matrices are those produced with uniform pipelines. All DCP Generated Matrices include quality control metrics.",
+        "The 10x matrices include raw counts. Smart-seq2 matrices include TPMs and estimated counts. Matrices are in Loom file format.",
+      ],
+      title: "DCP Generated Matrices",
     }),
-  } as ComponentConfig<typeof C.Text>,
+  } as ComponentConfig<typeof C.TitledText>,
 ];
