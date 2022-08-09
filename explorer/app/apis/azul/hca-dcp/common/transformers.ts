c9e815b6ae4e6f086af3865de66805c44e937ecb
Bruce Rodrigues <brucerodrigues6@gmail.com>
2022-08-09 19:28:12 -0300

    Fix #416

--- explorer/app/apis/azul/hca-dcp/common/transformers.ts
+++ explorer/app/apis/azul/hca-dcp/common/transformers.ts
@@ -3,22 +3,22 @@ import { concatStrings } from "../../../../utils/string";
 import { humanFileSize } from "../../../../utils/fileSize";
 
 export const filesToFileName = (file: FilesResponse): string =>
-  file.files[0].name ?? "";
+  file.files?.[0].name ?? "";
 
 export const filesToFileFormat = (file: FilesResponse): string =>
-  file.files[0].format ?? "";
+  file.files?.[0].format ?? "";
 
 export const filesToFileUrl = (file: FilesResponse): string =>
-  file.files[0].url ?? "";
+  file.files?.[0].url ?? "";
 
 export const filesToProjTitle = (file: FilesResponse): string =>
-  concatStrings(file.projects[0].projectTitle) ?? "";
+  concatStrings(file.projects?.[0].projectTitle ?? "");
 
 export const filesToFileSize = (file: FilesResponse): string =>
-  humanFileSize(file.files[0].size) ?? "";
+  humanFileSize(file.files?.[0].size ?? 0);
 
 export const filesToContentDesc = (file: FilesResponse): string =>
-  concatStrings(file.files[0].contentDescription) ?? "";
+  concatStrings(file.files?.[0].contentDescription ?? "");
 
 export const filesToCellCount = (file: FilesResponse): number =>
-  file.projects[0].estimatedCellCount ?? 0;
+  file.projects?.[0].estimatedCellCount ?? 0;
