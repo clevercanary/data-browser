b206301f793217f865176779d0b5404d204d6d0b
Esther McDade <esther@Esthers-MacBook-Pro.local>
2022-11-11 10:59:42 +1000

    Implement header nav "more" button. #591

--- explorer/app/components/common/Button/button.stories.tsx
+++ explorer/app/components/common/Button/button.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
 import { Button } from "./button";
 import { ButtonPrimary, ButtonSecondary } from "./button.styles";
+import { NavLinkDropdownButton } from "./components/NavLinkDropdownButton/navLinkDropdownButton";
 
 export default {
   argTypes: {
@@ -22,12 +23,21 @@ const SecondaryTemplate: ComponentStory<typeof Button> = (args) => (
   <ButtonSecondary {...args} />
 );
 
+const NavDropdownButtonTemplate: ComponentStory<typeof NavLinkDropdownButton> = (
+  args
+ ) => <NavLinkDropdownButton {...args} />;
+ 
 export const SecondaryButton = SecondaryTemplate.bind({});
 SecondaryButton.args = {
   children: "Secondary Button",
 };
-
+ 
 export const PrimaryButton = PrimaryTemplate.bind({});
 PrimaryButton.args = {
   children: "Primary Button",
+};
+
+export const NavDropdownButton = NavDropdownButtonTemplate.bind({});
+NavDropdownButton.args = {
+  children: "More",
 };
