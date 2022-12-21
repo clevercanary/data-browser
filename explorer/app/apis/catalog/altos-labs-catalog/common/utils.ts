bd7a7e307ec6876059c86675c27db9cd6cffb4f2
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2022-12-22 00:46:46 +1000

    Update Altos Labs to build with tsv. #174.

--- explorer/app/apis/catalog/altos-labs-catalog/common/utils.ts
+++ explorer/app/apis/catalog/altos-labs-catalog/common/utils.ts
@@ -1,196 +1,32 @@
 import {
-  accumulateValue,
-  accumulateValues,
-  sumValues,
-} from "../../common/utils";
-import {
-  AltosLabsCatalog,
-  AltosLabsCatalogConsortium,
-  AltosLabsCatalogStudy,
-  AltosLabsCatalogWorkspace,
-} from "./entities";
+  sanitizeString,
+  sanitizeStringArray,
+} from "../../../../viewModelBuilders/common/utils";
+import { AltosLabsCatalogExperiment } from "./entities";
 
 /**
- * Returns Altos Labs catalog consortia.
- * @param altosLabsCatalogs - Altos Labs catalogs.
- * @returns Altos Labs catalog consortia.
+ * Returns the Altos Labs catalog experiment sanitized.
+ * @param altosLabsCatalogExperiment - Experiment for sanitization.
+ * @returns sanitized Altos Labs catalog experiment.
  */
-export function buildAltosLabsCatalogConsortia(
-  altosLabsCatalogs: unknown[]
-): unknown[] {
-  const altosLabsCatalogConsortiaByConsortium = new Map();
-  const workspaces = buildAltosLabsCatalogWorkspaces(
-    altosLabsCatalogs
-  ) as AltosLabsCatalogWorkspace[];
-
-  // Build the workspaces for the consortium.
-  for (const workspace of workspaces) {
-    const { consortium } = workspace;
-    const altosLabsCatalogConsortium =
-      altosLabsCatalogConsortiaByConsortium.get(consortium) || {};
-    altosLabsCatalogConsortiaByConsortium.set(
-      consortium,
-      buildAltosLabsCatalogConsortium(workspace, altosLabsCatalogConsortium)
-    );
-  }
-
-  return [...altosLabsCatalogConsortiaByConsortium.values()];
-}
-
-/**
- * Returns Altos Labs catalog studies.
- * @param altosLabsCatalogs - Altos Labs catalogs.
- * @returns Altos Labs catalog studies.
- */
-export function buildAltosLabsCatalogStudies(
-  altosLabsCatalogs: unknown[]
-): unknown[] {
-  const studiesByStudyId = new Map();
-  const workspaces = buildAltosLabsCatalogWorkspaces(
-    altosLabsCatalogs
-  ) as AltosLabsCatalogWorkspace[];
-
-  // Build the studies.
-  for (const workspace of workspaces) {
-    const { dbGapId } = workspace;
-    if (isStudy(dbGapId)) {
-      const study = studiesByStudyId.get(dbGapId) || {};
-      studiesByStudyId.set(
-        dbGapId,
-        buildAltosLabsCatalogStudy(workspace, study)
-      );
-    }
-  }
-
-  return [...studiesByStudyId.values()];
-}
-
-/**
- * Returns Altos Labs catalog workspaces.
- * @param altosLabsCatalogs - Altos Labs catalogs.
- * @returns Altos Labs catalog workspaces.
- */
-export function buildAltosLabsCatalogWorkspaces(
-  altosLabsCatalogs: unknown[]
-): unknown[] {
-  return (altosLabsCatalogs as AltosLabsCatalog[]).map((altosLabsCatalog) => {
-    const workspace: AltosLabsCatalogWorkspace = {
-      consentCode: altosLabsCatalog["library:dataUseRestriction"],
-      consortium: altosLabsCatalog.consortium,
-      dataTypes: altosLabsCatalog["library:datatype"],
-      dbGapId: altosLabsCatalog.phsId,
-      diseases: altosLabsCatalog["library:indication"],
-      participantCount: altosLabsCatalog.participantCount,
-      studyDesigns: altosLabsCatalog["library:studyDesign"],
-      workspaceName: altosLabsCatalog.name,
-    };
-    return workspace;
-  });
-}
-
-/**
- * Returns Altos Labs catalog consortium.
- * @param workspace - Altos Labs catalog workspace.
- * @param altosLabsCatalogConsortium - Altos Labs catalog consortium.
- * @returns Altos Labs catalog consortium.
- */
-function buildAltosLabsCatalogConsortium(
-  workspace: AltosLabsCatalogWorkspace,
-  altosLabsCatalogConsortium: AltosLabsCatalogConsortium
-): AltosLabsCatalogConsortium {
-  const consentCodes = accumulateValue(
-    altosLabsCatalogConsortium.consentCode, // consentCodes - a list of consent codes.
-    workspace.consentCode
-  );
-  const consortium = workspace.consortium;
-  const dataTypes = accumulateValues(
-    altosLabsCatalogConsortium.dataTypes,
-    workspace.dataTypes
-  );
-  const dbGapId = accumulateValue(
-    altosLabsCatalogConsortium.dbGapId,
-    workspace.dbGapId
-  ); // dbGapIds - a list of study ids.
-  const diseases = accumulateValues(
-    altosLabsCatalogConsortium.diseases,
-    workspace.diseases
-  );
-  const participantCount = sumValues([
-    altosLabsCatalogConsortium.participantCount,
-    workspace.participantCount,
-  ]);
-  const studyDesigns = accumulateValues(
-    altosLabsCatalogConsortium.studyDesigns,
-    workspace.studyDesigns
-  );
-  const workspaceNames = accumulateValue(
-    altosLabsCatalogConsortium.workspaceName, // workspaceNames - a list of workspace names.
-    workspace.workspaceName
-  );
-  return {
-    consentCode: consentCodes,
-    consortium,
-    dataTypes,
-    dbGapId,
-    diseases,
-    participantCount,
-    studyDesigns,
-    workspaceCount: workspaceNames.length,
-    workspaceName: workspaceNames,
+export function AltosLabsExperimentInputMapper(
+  altosLabsCatalogExperiment: AltosLabsCatalogExperiment
+): AltosLabsCatalogExperiment {
+  const altosLabsExperiment: AltosLabsCatalogExperiment = {
+    doi: sanitizeString(altosLabsCatalogExperiment.doi),
+    shorthand: sanitizeString(altosLabsCatalogExperiment.shorthand),
+    species: sanitizeStringArray(altosLabsCatalogExperiment.species),
+    tissue: sanitizeStringArray(altosLabsCatalogExperiment.tissue),
+    title: sanitizeString(altosLabsCatalogExperiment.title),
   };
+  return altosLabsExperiment;
 }
 
 /**
- * Returns Altos Labs catalog study.
- * @param workspace - Altos Labs catalog workspace.
- * @param study - Altos Labs catalog study.
- * @returns Altos Labs catalog study.
+ * Returns the id of the experiment.
+ * @param altosLabsCatalogExperiment - Altos Lab catalog experiment.
+ * @returns String value of doi.
  */
-function buildAltosLabsCatalogStudy(
-  workspace: AltosLabsCatalogWorkspace,
-  study: AltosLabsCatalogStudy
-): AltosLabsCatalogStudy {
-  const consentCodes = accumulateValue(
-    study.consentCode, // consentCodes - a list of consent codes.
-    workspace.consentCode
-  );
-  const consortium = workspace.consortium;
-  const dataTypes = accumulateValues(study.dataTypes, workspace.dataTypes);
-  const dbGapId = workspace.dbGapId;
-  const diseases = accumulateValues(study.diseases, workspace.diseases);
-  const participantCount = sumValues([
-    study.participantCount,
-    workspace.participantCount,
-  ]);
-  const studyDesigns = accumulateValues(
-    study.studyDesigns,
-    workspace.studyDesigns
-  );
-  const workspaceNames = accumulateValue(
-    study.workspaceName, // workspaceNames - a list of workspace names.
-    workspace.workspaceName
-  );
-  return {
-    consentCode: consentCodes,
-    consortium,
-    dataTypes,
-    dbGapId,
-    diseases,
-    participantCount,
-    studyDesigns,
-    workspaceCount: workspaceNames.length,
-    workspaceName: workspaceNames,
-  };
-}
-
-/**
- * Returns true if study has a valid dbGapId.
- * @param dbGapId - study identifier.
- * @returns true if the study has a valid dbGapId.
- */
-function isStudy(dbGapId: string): boolean {
-  if (!dbGapId) {
-    return false;
-  }
-  return dbGapId.toLowerCase().startsWith("phs");
-}
+export const getDOIId = (
+  altosLabsCatalogExperiment: AltosLabsCatalogExperiment
+): string => altosLabsCatalogExperiment.doi || "";
--- explorer/app/viewModelBuilders/altos-labs-catalog/common/viewModelBuilders.ts
+++ explorer/app/viewModelBuilders/altos-labs-catalog/common/viewModelBuilders.ts
@@ -1,194 +1,74 @@
-import {
-  getConsentCode,
-  getConsentCodes,
-  getConsortium,
-  getDataTypes,
-  getDbGapId,
-  getDbGapIds,
-  getDiseases,
-  getParticipantCount,
-  getStudyDesigns,
-  getTerraWorkspaceCount,
-  getTerraWorkspaceName,
-  getTerraWorkspaceNames,
-} from "app/apis/catalog/altos-labs-catalog/common/transformers";
-import {
-  AltosLabsCatalogConsortium,
-  AltosLabsCatalogEntity,
-  AltosLabsCatalogStudy,
-  AltosLabsCatalogWorkspace,
-} from "../../../apis/catalog/altos-labs-catalog/common/entities";
+import { AltosLabsCatalogEntity } from "../../../apis/catalog/altos-labs-catalog/common/entities";
 import * as C from "../../../components";
 import { METADATA_KEY } from "../../../components/Index/common/entities";
 import { getPluralizedMetadataLabel } from "../../../components/Index/common/indexTransformer";
+import { ANCHOR_TARGET } from "../../../components/Links/common/entities";
 
 /**
- * Build props for consent code cell component from the given Altos Labs workspace.
- * @param altosLabsCatalogWorkspace - Altos Labs catalog workspace.
- * @returns Model to be used as props for the consent code cell.
+ * Build props for DOI cell component from the given Altos Labs entity.
+ * @param altosLabsCatalogEntity - Altos Labs catalog entity.
+ * @returns Model to be used as props for the DOI cell.
  */
-export const buildConsentCode = (
-  altosLabsCatalogWorkspace: AltosLabsCatalogWorkspace
-): React.ComponentProps<typeof C.Cell> => {
+export const buildDOI = (
+  altosLabsCatalogEntity: AltosLabsCatalogEntity
+): React.ComponentProps<typeof C.Link> => {
   return {
-    value: getConsentCode(altosLabsCatalogWorkspace),
+    label: altosLabsCatalogEntity.doi,
+    target: ANCHOR_TARGET.BLANK,
+    url: altosLabsCatalogEntity.doi,
   };
 };
 
 /**
- * Build props for consent codes cell component from the given Altos Labs entity.
+ * Build props for shorthand cell component from the given Altos Labs entity.
  * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the consent codes cell.
+ * @returns Model to be used as props for the shorthand cell.
  */
-export const buildConsentCodes = (
-  altosLabsCatalogEntity: Exclude<
-    AltosLabsCatalogEntity,
-    AltosLabsCatalogWorkspace
-  >
-): React.ComponentProps<typeof C.NTagCell> => {
-  return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.CONSENT_CODE),
-    values: getConsentCodes(altosLabsCatalogEntity),
-  };
-};
-
-/**
- * Build props for consortium cell component from the given Altos Labs entity.
- * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the consortium cell.
- */
-export const buildConsortium = (
+export const buildShorthand = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.Cell> => {
   return {
-    value: getConsortium(altosLabsCatalogEntity),
+    value: altosLabsCatalogEntity.shorthand,
   };
 };
 
 /**
- * Build props for data type cell component from the given Altos Labs entity.
+ * Build props for species cell component from the given Altos Labs entity.
  * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the data type cell.
+ * @returns Model to be used as props for the species cell.
  */
-export const buildDataTypes = (
+export const buildSpecies = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.NTagCell> => {
   return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.DATA_TYPE),
-    values: getDataTypes(altosLabsCatalogEntity),
+    label: getPluralizedMetadataLabel(METADATA_KEY.SPECIES),
+    values: altosLabsCatalogEntity.species,
   };
 };
 
 /**
- * Build props for dbGapId cell component from the given Altos Labs entity.
+ * Build props for tissue cell component from the given Altos Labs entity.
  * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the dbGapId cell.
+ * @returns Model to be used as props for the tissue cell.
  */
-export const buildDbGapId = (
-  altosLabsCatalogEntity: Exclude<
-    AltosLabsCatalogEntity,
-    AltosLabsCatalogConsortium
-  >
-): React.ComponentProps<typeof C.Cell> => {
-  return {
-    value: getDbGapId(altosLabsCatalogEntity),
-  };
-};
-
-/**
- * Build props for dbGapIds cell component from the given Altos Labs consortium catalog.
- * @param altosLabsCatalogConsortium - Altos Labs catalog consortium.
- * @returns Model to be used as props for the dbGapIds cell.
- */
-export const buildDbGapIds = (
-  altosLabsCatalogConsortium: AltosLabsCatalogConsortium
-): React.ComponentProps<typeof C.NTagCell> => {
-  return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.DBGAP_ID),
-    values: getDbGapIds(altosLabsCatalogConsortium),
-  };
-};
-
-/**
- * Build props for disease (indication) cell component from the given Altos Labs entity.
- * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the disease (indication) cell.
- */
-export const buildDiseases = (
+export const buildTissue = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.NTagCell> => {
   return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.DISEASE_INDICATION),
-    values: getDiseases(altosLabsCatalogEntity),
+    label: getPluralizedMetadataLabel(METADATA_KEY.TISSUE),
+    values: altosLabsCatalogEntity.tissue,
   };
 };
 
 /**
- * Build props for participant count cell component from the given Altos Labs entity.
+ * Build props for title cell component from the given Altos Labs entity.
  * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the participant count cell.
+ * @returns Model to be used as props for the title cell.
  */
-export const buildParticipantCount = (
+export const buildTitle = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.Cell> => {
   return {
-    value: getParticipantCount(altosLabsCatalogEntity),
-  };
-};
-
-/**
- * Build props for study design cell component from the given Altos Labs entity.
- * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the study design cell.
- */
-export const buildStudyDesigns = (
-  altosLabsCatalogEntity: AltosLabsCatalogEntity
-): React.ComponentProps<typeof C.NTagCell> => {
-  return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.STUDY_DESIGN),
-    values: getStudyDesigns(altosLabsCatalogEntity),
-  };
-};
-
-/**
- * Build props for terra workspace count cell component from the given Altos Labs study.
- * @param altosLabsCatalogStudy - Altos Labs catalog study.
- * @returns Model to be used as props for the terra workspace count cell.
- */
-export const buildTerraWorkspaceCount = (
-  altosLabsCatalogStudy: AltosLabsCatalogStudy
-): React.ComponentProps<typeof C.Cell> => {
-  return {
-    value: getTerraWorkspaceCount(altosLabsCatalogStudy),
-  };
-};
-
-/**
- * Build props for terra workspace name cell component from the given Altos Labs workspace.
- * @param altosLabsCatalogWorkspace - Altos Labs catalog workspace.
- * @returns Model to be used as props for the terra workspace name cell.
- */
-export const buildTerraWorkspaceName = (
-  altosLabsCatalogWorkspace: AltosLabsCatalogWorkspace
-): React.ComponentProps<typeof C.Cell> => {
-  return {
-    value: getTerraWorkspaceName(altosLabsCatalogWorkspace),
-  };
-};
-
-/**
- * Build props for terra workspace names cell component from the given Altos Labs entity.
- * @param altosLabsCatalogEntity - Altos Labs catalog entity.
- * @returns Model to be used as props for the terra workspace names cell.
- */
-export const buildTerraWorkspaceNames = (
-  altosLabsCatalogEntity: Exclude<
-    AltosLabsCatalogEntity,
-    AltosLabsCatalogWorkspace
-  >
-): React.ComponentProps<typeof C.NTagCell> => {
-  return {
-    label: getPluralizedMetadataLabel(METADATA_KEY.WORKSPACE_NAME),
-    values: getTerraWorkspaceNames(altosLabsCatalogEntity),
+    value: altosLabsCatalogEntity.title,
   };
 };
