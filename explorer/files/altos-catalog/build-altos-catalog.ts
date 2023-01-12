eacf0c55a4ed2d4cb296c7259bd622a18fd4a563
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-01-12 15:17:07 -0800

    feat: add files entity list (altos#11)

--- explorer/files/altos-catalog/build-altos-catalog.ts
+++ explorer/files/altos-catalog/build-altos-catalog.ts
@@ -1,4 +1,7 @@
-import { AltosLabsCatalog } from "../../app/apis/catalog/altos-labs-catalog/common/entities";
+import {
+  AltosLabsCatalog,
+  AltosLabsCatalogFile,
+} from "../../app/apis/catalog/altos-labs-catalog/common/entities";
 import {
   parseContentRows,
   parseDatumValue,
@@ -7,13 +10,16 @@ import {
 import { writeAsJSON } from "../common/utils";
 import { buildAltosLabCatalogExperiment } from "./build-experiment";
 import {
+  FILES_SOURCE_FIELD_KEY,
+  FILES_SOURCE_FIELD_PROPERTY,
+  FILES_SOURCE_FIELD_TYPE,
   FILE_NAME_TYPE,
   SOURCE_FIELD_KEY,
   SOURCE_FIELD_PROPERTY,
   SOURCE_FIELD_TYPE,
 } from "./constants";
 import {
-  AltosLabsCatalogFile,
+  AltosLabsCatalogFile as AltosLabsCatalogSourceFile,
   ExperimentTypeKeyFileName,
   EXPERIMENT_TYPE,
 } from "./entities";
@@ -22,7 +28,8 @@ import { TaxonomySpecies } from "./taxonomy";
 console.log("Building Altos Catalog Data");
 export {};
 
-const tsvPaths = [
+const filesTsvPath = "altos-catalog/files/files.tsv";
+const datasetsTsvPaths = [
   "altos-catalog/files/public-datasets-perturbational.tsv",
   "altos-catalog/files/public-datasets-reprogramming.tsv",
 ];
@@ -35,7 +42,7 @@ async function buildCatalog(): Promise<void> {
   const experimentTypeByFileName = getExperimentTypeByFileName();
   // Read and parse the raw tsv files.
   const altosLabsCatalogs = [] as AltosLabsCatalog[];
-  for (const path of tsvPaths) {
+  for (const path of datasetsTsvPaths) {
     // Read the file.
     const file = await readFile(path);
     if (!file) {
@@ -47,7 +54,7 @@ async function buildCatalog(): Promise<void> {
       "\t",
       SOURCE_FIELD_KEY,
       SOURCE_FIELD_TYPE
-    )) as AltosLabsCatalogFile[];
+    )) as AltosLabsCatalogSourceFile[];
     // Determine experiment type.
     const experimentType = experimentTypeByFileName.get(path);
     if (!experimentType) {
@@ -58,20 +65,79 @@ async function buildCatalog(): Promise<void> {
     altosLabsCatalogs.push(...catalogs);
   }
 
+  const altosLabsCatalogFile = await buildAltosLabsFiles(altosLabsCatalogs);
+
   /**
    * Build the experiment catalog.
    */
-  const altosLabsCatalogExperiment =
-    buildAltosLabCatalogExperiment(altosLabsCatalogs);
+  const altosLabsCatalogExperiment = buildAltosLabCatalogExperiment(
+    altosLabsCatalogs,
+    altosLabsCatalogFile
+  );
 
   /**
    * Write out the resulting files.
    */
+  console.log("File:", altosLabsCatalogFile.length);
+  await writeAsJSON(
+    "altos-catalog/out/altos-catalog-file.json",
+    Object.fromEntries(altosLabsCatalogFile.entries())
+  );
   console.log("Experiment:", altosLabsCatalogExperiment.length);
   await writeAsJSON(
     "altos-catalog/out/altos-catalog-experiment.json",
     Object.fromEntries(altosLabsCatalogExperiment.entries())
   );
+}
+
+/**
+ * Returns Altos Labs files.
+ * @param altosLabsCatalogs - Altos Labs experiment catalogs.
+ * @returns Altos Labs files.
+ */
+async function buildAltosLabsFiles(
+  altosLabsCatalogs: AltosLabsCatalog[]
+): Promise<AltosLabsCatalogFile[]> {
+  // Map shorthand to experiment object.
+  const experimentByShorthand = getExperimentByShorthand(altosLabsCatalogs);
+  // Read the file.
+  const file = await readFile(filesTsvPath);
+  if (!file) {
+    throw new Error(`File ${filesTsvPath} not found`);
+  }
+  // Parse file contents.
+  const rows = (await parseContentRows(
+    file,
+    "\t",
+    FILES_SOURCE_FIELD_KEY,
+    FILES_SOURCE_FIELD_TYPE
+  )) as AltosLabsCatalogSourceFile[];
+  // Consolidate contents into desired Altos Labs shape.
+  return rows.map((r) => {
+    const row = {} as AltosLabsCatalogFile;
+    for (const [fieldKey, rowKey] of Object.entries(FILES_SOURCE_FIELD_KEY)) {
+      const key =
+        FILES_SOURCE_FIELD_PROPERTY[
+          fieldKey as keyof typeof FILES_SOURCE_FIELD_KEY
+        ];
+      let value = r[rowKey as keyof AltosLabsCatalogSourceFile] || "";
+      if (!value) {
+        const fieldType =
+          FILES_SOURCE_FIELD_TYPE[
+            fieldKey as keyof typeof FILES_SOURCE_FIELD_KEY
+          ];
+        value = parseDatumValue(
+          value,
+          fieldType
+        ) as unknown as keyof typeof FILES_SOURCE_FIELD_PROPERTY;
+      }
+      Object.assign(row, { [key]: value });
+    }
+    return {
+      ...experimentByShorthand[row.shorthand],
+      ...row,
+    };
+  });
 }
 
 /**
@@ -81,7 +147,7 @@ async function buildCatalog(): Promise<void> {
  * @returns Altos Labs Catalog.
  */
 function buildAltosLabsCatalog(
-  rows: AltosLabsCatalogFile[],
+  rows: AltosLabsCatalogSourceFile[],
   experimentType: EXPERIMENT_TYPE
 ): AltosLabsCatalog[] {
   return rows.map((r) => {
@@ -89,7 +155,7 @@ function buildAltosLabsCatalog(
     for (const [fieldKey, rowKey] of Object.entries(SOURCE_FIELD_KEY)) {
       const key =
         SOURCE_FIELD_PROPERTY[fieldKey as keyof typeof SOURCE_FIELD_KEY];
-      let value = r[rowKey as keyof AltosLabsCatalogFile] || "";
+      let value = r[rowKey as keyof AltosLabsCatalogSourceFile] || "";
       if (!value) {
         const fieldType =
           SOURCE_FIELD_TYPE[fieldKey as keyof typeof SOURCE_FIELD_KEY];
@@ -105,14 +171,29 @@ function buildAltosLabsCatalog(
     );
     return {
       ...row,
-      species,
       ...{
         description: "None", // TODO description
         experimentType,
         initiative: "APP",
+        species,
       },
     };
   });
+}
+
+/**
+ * Returns an object mapping shorthand to experiment object.
+ * @param experiments - Altos Labs experiments.
+ * @returns object mapping shorthand to experiment object.
+ */
+function getExperimentByShorthand(
+  experiments: AltosLabsCatalog[]
+): Record<string, AltosLabsCatalog> {
+  const experimentsByShorthand: Record<string, AltosLabsCatalog> = {};
+  for (const experiment of experiments) {
+    experimentsByShorthand[experiment.shorthand] = experiment;
+  }
+  return experimentsByShorthand;
 }
 
 /**
--- explorer/site-config/altos-labs-catalog/filter-category-keys.ts
+++ explorer/site-config/altos-labs-catalog/filter-category-keys.ts
@@ -2,6 +2,8 @@ export const ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS = {
   ASSAY: "assay",
   DOI: "doi",
   EXPERIMENT_TYPE: "experimentType",
+  FILE_PATH: "filePath",
+  FILE_TYPE: "fileType",
   INITIATIVE: "initiative",
   SHORTHAND: "shorthand",
   SPECIES: "species",
