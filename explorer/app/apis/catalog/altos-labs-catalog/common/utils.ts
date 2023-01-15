30939ffabfbac7738d2b0747ffe41ba04e7e5ab0
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2023-01-14 22:42:47 -0800

    Add files entity list (#654)
    * feat: add files entity list (altos#11)
    
    * refactor: use a function instead of duplicating spreadsheet conversion code (altos#11)
    
    * refactor: delete unused interface (altos#11)

--- explorer/app/apis/catalog/altos-labs-catalog/common/utils.ts
+++ explorer/app/apis/catalog/altos-labs-catalog/common/utils.ts
@@ -2,7 +2,11 @@ import {
   sanitizeString,
   sanitizeStringArray,
 } from "../../../../viewModelBuilders/common/utils";
-import { AltosLabsCatalogExperiment } from "./entities";
+import {
+  AltosLabsCatalogEntity,
+  AltosLabsCatalogExperiment,
+  AltosLabsCatalogFile,
+} from "./entities";
 
 /**
  * Returns the Altos Labs catalog experiment sanitized.
@@ -13,10 +17,14 @@ export function AltosLabsExperimentInputMapper(
   altosLabsCatalogExperiment: AltosLabsCatalogExperiment
 ): AltosLabsCatalogExperiment {
   const altosLabsExperiment: AltosLabsCatalogExperiment = {
+    NCBITaxonomyID: sanitizeStringArray(
+      altosLabsCatalogExperiment.NCBITaxonomyID
+    ),
     assay: sanitizeStringArray(altosLabsCatalogExperiment.assay),
     description: altosLabsCatalogExperiment.description,
     doi: sanitizeString(altosLabsCatalogExperiment.doi),
     experimentType: altosLabsCatalogExperiment.experimentType,
+    files: sanitizeStringArray(altosLabsCatalogExperiment.files),
     initiative: altosLabsCatalogExperiment.initiative, // Expected value "APP".
     shorthand: sanitizeString(altosLabsCatalogExperiment.shorthand),
     species: sanitizeStringArray(altosLabsCatalogExperiment.species),
@@ -27,10 +35,34 @@ export function AltosLabsExperimentInputMapper(
 }
 
 /**
- * Returns the id of the experiment.
- * @param altosLabsCatalogExperiment - Altos Lab catalog experiment.
+ * Returns the Altos Labs catalog file info sanitized.
+ * @param altosLabsCatalogFile - File info for sanitization.
+ * @returns sanitized Altos Labs catalog file info.
+ */
+export function AltosLabsFileInputMapper(
+  altosLabsCatalogFile: AltosLabsCatalogFile
+): AltosLabsCatalogFile {
+  const altosLabsFile: AltosLabsCatalogFile = {
+    assay: sanitizeStringArray(altosLabsCatalogFile.assay),
+    description: altosLabsCatalogFile.description,
+    doi: sanitizeString(altosLabsCatalogFile.doi),
+    experimentType: altosLabsCatalogFile.experimentType,
+    filePath: sanitizeString(altosLabsCatalogFile.filePath),
+    fileType: sanitizeString(altosLabsCatalogFile.fileType),
+    initiative: altosLabsCatalogFile.initiative, // Expected value "APP".
+    shorthand: sanitizeString(altosLabsCatalogFile.shorthand),
+    species: sanitizeStringArray(altosLabsCatalogFile.species),
+    tissue: sanitizeStringArray(altosLabsCatalogFile.tissue),
+    title: sanitizeString(altosLabsCatalogFile.title),
+  };
+  return altosLabsFile;
+}
+
+/**
+ * Returns the id of the entity.
+ * @param altosLabsCatalogEntity - Altos Lab catalog entity.
  * @returns String value of shorthand.
  */
 export const getShorthandId = (
-  altosLabsCatalogExperiment: AltosLabsCatalogExperiment
-): string => altosLabsCatalogExperiment.shorthand || "";
+  altosLabsCatalogEntity: AltosLabsCatalogEntity
+): string => altosLabsCatalogEntity.shorthand || "";
