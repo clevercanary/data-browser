0e7c96dda0da029e9656c8ae6bd23a2487b41598
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2023-01-14 22:42:47 -0800

    Add files entity list (#654)
    * feat: add files entity list (altos#11)
    
    * refactor: use a function instead of duplicating spreadsheet conversion code (altos#11)
    
    * refactor: delete unused interface (altos#11)

--- explorer/files/altos-catalog/build-experiment.ts
+++ explorer/files/altos-catalog/build-experiment.ts
@@ -1,15 +1,47 @@
 import {
   AltosLabsCatalog,
   AltosLabsCatalogExperiment,
+  AltosLabsCatalogFile,
 } from "../../app/apis/catalog/altos-labs-catalog/common/entities";
 
 /**
  * Returns Altos Labs catalog experiment.
  * @param altosLabsCatalogs - Altos Labs Catalog.
+ * @param altosLabsCatalogFile - Altos Labs files.
  * @returns Altos Labs catalog experiment.
  */
 export function buildAltosLabCatalogExperiment(
-  altosLabsCatalogs: AltosLabsCatalog[]
+  altosLabsCatalogs: AltosLabsCatalog[],
+  altosLabsCatalogFile: AltosLabsCatalogFile[]
 ): AltosLabsCatalogExperiment[] {
-  return altosLabsCatalogs as AltosLabsCatalogExperiment[];
+  const filesByShorthand = getFilesByShorthand(altosLabsCatalogFile);
+
+  return altosLabsCatalogs.map(
+    (experiment: AltosLabsCatalog): AltosLabsCatalogExperiment => {
+      return {
+        ...experiment,
+        ...{
+          files: filesByShorthand[experiment.shorthand] || [],
+        },
+      };
+    }
+  );
+}
+
+/**
+ * Returns an object mapping shorthand to file paths array.
+ * @param files - Altos Labs files.
+ * @returns object mapping shorthand to file paths array.
+ */
+function getFilesByShorthand(
+  files: AltosLabsCatalogFile[]
+): Record<string, string[]> {
+  const filesByShorthand: Record<string, string[]> = {};
+  for (const file of files) {
+    (
+      filesByShorthand[file.shorthand] ||
+      (filesByShorthand[file.shorthand] = [])
+    ).push(file.filePath);
+  }
+  return filesByShorthand;
 }
---
+++ explorer/site-config/altos-labs-catalog/dev/index/fileEntityConfig.ts
@@ -0,0 +1,154 @@
+import { AltosLabsCatalogFile } from "../../../../app/apis/catalog/altos-labs-catalog/common/entities";
+import {
+  AltosLabsFileInputMapper,
+  getShorthandId,
+} from "../../../../app/apis/catalog/altos-labs-catalog/common/utils";
+import * as Components from "../../../../app/components";
+import {
+  ComponentConfig,
+  EntityConfig,
+  ListConfig,
+} from "../../../../app/config/common/entities";
+import * as ViewBuilder from "../../../../app/viewModelBuilders/catalog/altos-labs-catalog/common/viewModelBuilders";
+import { ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS } from "../../filter-category-keys";
+
+/**
+ * Entity config object responsible to config anything related to the /explore/files route.
+ */
+export const fileEntity: EntityConfig<AltosLabsCatalogFile> = {
+  detail: {
+    detailOverviews: [],
+    staticLoad: false,
+    tabs: [],
+    top: [],
+  },
+  getId: getShorthandId,
+  label: "Files",
+  list: {
+    columns: [
+      {
+        componentConfig: {
+          component: Components.Cell,
+          viewBuilder: ViewBuilder.buildFilePath,
+        } as ComponentConfig<typeof Components.Cell>,
+        header: "File path",
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.FILE_PATH,
+        },
+        width: { max: "1fr", min: "160px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Cell,
+          viewBuilder: ViewBuilder.buildFileType,
+        } as ComponentConfig<typeof Components.Cell>,
+        header: "File type",
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.FILE_TYPE,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Cell,
+          viewBuilder: ViewBuilder.buildInitiative,
+        } as ComponentConfig<typeof Components.Cell>,
+        header: "Initiative",
+        hiddenColumn: true,
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.INITIATIVE,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Cell,
+          viewBuilder: ViewBuilder.buildShorthand,
+        } as ComponentConfig<typeof Components.Cell>,
+        header: "Shorthand",
+        sort: {
+          default: true,
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.SHORTHAND,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Link,
+          viewBuilder: ViewBuilder.buildTitle,
+        } as ComponentConfig<typeof Components.Link>,
+        header: "Experiment",
+        sort: {
+          default: true,
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.TITLE,
+        },
+        width: { max: "1.5fr", min: "200px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Link,
+          viewBuilder: ViewBuilder.buildDOI,
+        } as ComponentConfig<typeof Components.Link>,
+        header: "DOI",
+        hiddenColumn: true,
+        sort: {
+          default: true,
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.DOI,
+        },
+        width: { max: "1fr", min: "160px" },
+      },
+      {
+        componentConfig: {
+          component: Components.NTagCell,
+          viewBuilder: ViewBuilder.buildSpecies,
+        } as ComponentConfig<typeof Components.NTagCell>,
+        header: "Species",
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.SPECIES,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+      {
+        componentConfig: {
+          component: Components.NTagCell,
+          viewBuilder: ViewBuilder.buildTissue,
+        } as ComponentConfig<typeof Components.NTagCell>,
+        header: "Tissue",
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.TISSUE,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+      {
+        componentConfig: {
+          component: Components.Cell,
+          viewBuilder: ViewBuilder.buildExperimentType,
+        } as ComponentConfig<typeof Components.Cell>,
+        header: "Experiment Type",
+        hiddenColumn: true,
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.EXPERIMENT_TYPE,
+        },
+        width: { max: "1fr", min: "160px" },
+      },
+      {
+        componentConfig: {
+          component: Components.NTagCell,
+          viewBuilder: ViewBuilder.buildAssay,
+        } as ComponentConfig<typeof Components.NTagCell>,
+        header: "Assay",
+        sort: {
+          sortKey: ALTOS_LABS_CATALOG_FILTER_CATEGORY_KEYS.ASSAY,
+        },
+        width: { max: "0.5fr", min: "132px" },
+      },
+    ],
+  } as ListConfig<AltosLabsCatalogFile>,
+  listView: {
+    disablePagination: true,
+  },
+  route: "files",
+  staticEntityImportMapper: AltosLabsFileInputMapper,
+  staticLoad: true,
+  staticLoadFile: "files/altos-catalog/out/altos-catalog-file.json",
+};
