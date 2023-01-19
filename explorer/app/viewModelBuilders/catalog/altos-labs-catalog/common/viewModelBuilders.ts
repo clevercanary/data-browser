ca71fc51d5276dc09400974d95e1a91db59b1761
Fran McDade <frano-m@users.noreply.github.com>
2023-01-19 15:08:24 +1000

    feat: make file url link to AWS console (#13) (#655)

--- explorer/app/viewModelBuilders/catalog/altos-labs-catalog/common/viewModelBuilders.ts
+++ explorer/app/viewModelBuilders/catalog/altos-labs-catalog/common/viewModelBuilders.ts
@@ -16,6 +16,12 @@ import { getPluralizedMetadataLabel } from "../../../../components/Index/common/
 import { ANCHOR_TARGET } from "../../../../components/Links/common/entities";
 import { getEntityConfig } from "../../../../config/config";
 
+const S3_URI = {
+  DIRECTORY_URL:
+    "https://s3.console.aws.amazon.com/s3/buckets/altos-lab-project-app",
+  FILE_URL: "https://s3.console.aws.amazon.com/s3/object/altos-lab-project-app",
+};
+
 /**
  * Build props for assay cell component from the given Altos Labs entity.
  * @param altosLabsCatalogEntity - Altos Labs catalog entity.
@@ -39,9 +45,9 @@ export const buildDOI = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.Link> => {
   return {
-    label: altosLabsCatalogEntity.doi,
+    label: altosLabsCatalogEntity.DOI,
     target: ANCHOR_TARGET.BLANK,
-    url: altosLabsCatalogEntity.doi,
+    url: altosLabsCatalogEntity.DOI,
   };
 };
 
@@ -66,13 +72,13 @@ export const buildExperimentDescription = (
 export const buildExperimentDetails = (
   altosLabsCatalogEntity: AltosLabsCatalogEntity
 ): React.ComponentProps<typeof C.KeyValuePairs> => {
-  const { assay, doi, experimentType, shorthand, species, tissue } =
+  const { assay, DOI, experimentType, shorthand, species, tissue } =
     altosLabsCatalogEntity;
   const keyValuePairs = new Map<Key, Value>();
   keyValuePairs.set("Assay", stringifyValues(assay));
   keyValuePairs.set(
-    "Doi",
-    C.Link({ label: doi, target: ANCHOR_TARGET.BLANK, url: doi })
+    "DOI",
+    C.Link({ label: DOI, target: ANCHOR_TARGET.BLANK, url: DOI })
   );
   keyValuePairs.set("Experiment Type", experimentType);
   keyValuePairs.set("Shorthand", shorthand);
@@ -117,19 +123,6 @@ export const buildExperimentType = (
 };
 
 /**
- * Build props for file path cell component from the given Altos Labs entity.
- * @param altosLabsCatalogFile - Altos Labs catalog file entity.
- * @returns Model to be used as props for the file path cell.
- */
-export const buildFilePath = (
-  altosLabsCatalogFile: AltosLabsCatalogFile
-): React.ComponentProps<typeof C.Cell> => {
-  return {
-    value: altosLabsCatalogFile.filePath,
-  };
-};
-
-/**
  * Build props for file type cell component from the given Altos Labs entity.
  * @param altosLabsCatalogFile - Altos Labs catalog file entity.
  * @returns Model to be used as props for the file type cell.
@@ -152,6 +145,32 @@ export const buildInitiative = (
 ): React.ComponentProps<typeof C.Cell> => {
   return {
     value: altosLabsCatalogEntity.initiative,
+  };
+};
+
+/**
+ * Build props for S3URI cell component from the given Altos Labs entity.
+ * @param altosLabsCatalogFile - Altos Labs catalog file entity.
+ * @returns Model to be used as props for the S3URI cell.
+ */
+export const buildS3URI = (
+  altosLabsCatalogFile: AltosLabsCatalogFile
+): React.ComponentProps<typeof C.Link> => {
+  const { S3URI } = altosLabsCatalogFile;
+  if (/^s3:\/\//g.test(S3URI)) {
+    const S3URIPathname = getS3URIPathname(S3URI);
+    if (S3URIPathname) {
+      const S3URL = getS3URIUrl(S3URIPathname);
+      return {
+        label: S3URI,
+        target: ANCHOR_TARGET.BLANK,
+        url: S3URL.href,
+      };
+    }
+  }
+  return {
+    label: S3URI,
+    url: "",
   };
 };
 
@@ -231,4 +250,44 @@ function getCatalogBreadcrumbs(
     breadcrumbs.push({ path: "", text: lastCrumbText });
   }
   return breadcrumbs;
+}
+
+/**
+ * Returns the S3URI pathname from the S3URI.
+ * @param S3URI - S3URI.
+ * @returns the S3URI pathname.
+ */
+function getS3URIPathname(S3URI: string): string | undefined {
+  const matches = S3URI.match(/(\/\/.*?\/)(?<prefix>.*)/);
+  return matches?.groups?.prefix;
+}
+
+/**
+ * Returns the S3 url for the specified S3URI pathname and appends any relevant search parameters.
+ * @param pathname - S3URI pathname.
+ * @returns the S3 url.
+ */
+function getS3URIUrl(pathname: string): URL {
+  const pathnameIsDirectory = isDirectory(pathname);
+  let S3URL;
+  if (pathnameIsDirectory) {
+    S3URL = new URL(S3_URI.DIRECTORY_URL);
+  } else {
+    S3URL = new URL(S3_URI.FILE_URL);
+  }
+  S3URL.searchParams.append("region", "us-west-2");
+  S3URL.searchParams.append("prefix", pathname);
+  if (pathnameIsDirectory) {
+    S3URL.searchParams.append("showversions", "false");
+  }
+  return S3URL;
+}
+
+/**
+ * Returns true if the pathname is a directory.
+ * @param pathname - Pathname.
+ * @returns true if the pathname is a directory.
+ */
+function isDirectory(pathname: string): boolean {
+  return !/\w+\.\w+$/g.test(pathname);
 }
