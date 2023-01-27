ef9089dd01df2e63211be3d3b0ab13d0cada06e4
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-01-27 20:45:02 +1000

    feat: make all filters show all the time (#657)

--- explorer/app/components/TableCreator/definitions.d.ts
+++ explorer/app/components/TableCreator/definitions.d.ts
@@ -2,6 +2,7 @@ import { RowData } from "@tanstack/react-table";
 import { GridTrackSize } from "../../config/common/entities";
 
 declare module "@tanstack/table-core" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- TODO revisit nounused variables
   interface ColumnMeta<TData extends RowData, TValue> {
     width: GridTrackSize;
   }
