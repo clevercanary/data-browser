57709bef52e4278ba1b51e27ae9f084d08a14253
Fran McDade <frano-m@users.noreply.github.com>
2023-01-31 17:27:30 +1000

    refactor: refactor column config and list config sorting fields (#27) (#666)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ explorer/site-config/altos-labs-catalog/category.ts
@@ -0,0 +1,25 @@
+export const ALTOS_LABS_CATALOG_CATEGORY_KEY = {
+  ASSAY: "assay",
+  DOI: "doi",
+  EXPERIMENT: "title",
+  EXPERIMENT_TYPE: "experimentType",
+  FILE_TYPE: "fileType",
+  INITIATIVE: "initiative",
+  S3_URI: "s3Uri",
+  SHORTHAND: "shorthand",
+  SPECIES: "species",
+  TISSUE: "tissue",
+};
+
+export const ALTOS_LABS_CATALOG_CATEGORY_LABEL = {
+  ASSAY: "Assay",
+  DOI: "DOI",
+  EXPERIMENT: "Experiment",
+  EXPERIMENT_TYPE: "Experiment Type",
+  FILE_TYPE: "File Type",
+  INITIATIVE: "Initiative",
+  S3_URI: "S3 URI",
+  SHORTHAND: "Shorthand",
+  SPECIES: "Species",
+  TISSUE: "Tissue",
+};
